3b94887c1b34159eb36e0d10b2bd360f3733116c
dainahwanjiku <deeshish2017@gmail.com>
2022-06-01 10:15:08 -0700

    files

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 print(*["%c" % a for a in range(97, 123)
-            if "%c" % a not in 'qe'.format(a)], sep='', end='')
+        if "%c" % a not in 'qe'.format(a)], sep='', end='')
