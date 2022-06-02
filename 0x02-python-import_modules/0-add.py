e4fcd758c70967cf360012ca02f783c6a8f9954d
dainahwanjiku <deeshish2017@gmail.com>
2022-06-02 14:58:44 -0700

    modules

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+a = 1
+b = 2
+def add(a, b):
+    # This program adds two numbers and return the result.
+    print("the addition function".format)
+    result = a + b
+    return result
