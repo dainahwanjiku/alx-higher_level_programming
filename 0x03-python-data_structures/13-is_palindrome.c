981b9b180c75d311b54f02a0be3e8074659ddb16
dainahwanjiku <deeshish2017@gmail.com>
2022-06-07 21:13:47 +0300

    data structures

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,20 +1,15 @@
-#!/usr/bin/python3
+#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 
- 
-# comments are made here
-
-int is_palindrome(listint_t **head)
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
-    const listint_t *current;
-    const listint_t *tail;
-    unsigned int n = 0;
+	listint_t *head;
 
-    //Get length
-    //Use a function to get a node on a specific index
-    //check the first to the last and, second to second last ....
-    //return false if one check was False
-    //return true at the end (if all passed the checks)
-
-    return (n);
+	return (0);
 }
