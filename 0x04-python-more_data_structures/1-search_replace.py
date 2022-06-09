bc8cd0e0a894b633af7e292de201386de90bb35c
dainahwanjiku <deeshish2017@gmail.com>
2022-06-08 17:11:56 -0700

    more data structures

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    square_matrix = []
-    for elm in matrix:
-        square_matrix.append([c**2 for c in elm])
-    return square_matrix
+def search_replace(my_list, search, replace):
+    return [replace if search ==  else b for b in my_list]
