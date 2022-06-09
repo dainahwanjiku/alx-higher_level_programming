e22e849e2aa07dc85c846fabf3aeaf65e9355c72
dainahwanjiku <deeshish2017@gmail.com>
2022-06-08 17:23:12 -0700

    more data structures

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    return [replace if search ==  else b for b in my_list]
+    return [replace if search == b else b for b in my_list]
