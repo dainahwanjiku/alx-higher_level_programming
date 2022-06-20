43f64062592aa75603ea89bcfda52979ac881df0
dainahwanjiku <deeshish2017@gmail.com>
2022-06-20 15:08:22 -0700

    files

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -4,7 +4,7 @@ def safe_print_integer(value):
 
     try:
         print("{:d}\n" .format(value))
-    except Exception as error:
+    except (ValueError, TypeError):
         return False
     else:
         return True
