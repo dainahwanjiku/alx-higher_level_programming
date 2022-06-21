c2c630f0f924dd7c59698d61ee4a052736614197
dainahwanjiku <deeshish2017@gmail.com>
2022-06-22 02:26:00 +0300

    files

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
+"""
+this is a class square
 
-"""this is a class square"""
+it implements value and type checks for its attributes
+"""
 
 
 class Square:
-    """implementation"""
-    def__init__(self, size=0):
+    """
+    implementation
+    """
+    def __init__(self, size=0):
         if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
