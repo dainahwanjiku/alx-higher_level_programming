53d99e5e634f1f65d70079cd0baaa2b70e5ab948
dainahwanjiku <deeshish2017@gmail.com>
2022-06-22 03:59:44 +0300

    files

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""
+This module defines a Square class
+Its implements value and type checks for its attributes with area function
+"""
+
+
+class Square:
+    """Square implementation
+    """
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if type(size) != int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    def area(self):
+        return (self.__size ** 2)
+
+    def my_print(self):
+        """prints the square
+        if self.__size == 0:
+            print()
+            return
+        for i in range(self.__size):
+            print("".join(["#" for j in range(self.__size)]))
