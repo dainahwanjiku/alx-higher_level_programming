665e4e5791f2d4b48327fd587590ed89130986b2
dainahwanjiku <deeshish2017@gmail.com>
2022-06-28 05:00:58 +0300

    defined rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,32 +8,32 @@ class Rectangle:
     """
     implementation of the rectangle shape
     """
-    #def __init__(self, width=0, height=0):#
+    def __init__(self, width=0, height=0):
 
     @property
     def width(self):
-        self.width = width
+        self.__width = width
 
     @property setter
     def width(self, value):
-        self.value = value
+        self.__value = value
         if width != int:
             raise TypeError("width must be an integer")
         elif width < 0:
             raise ValueError("width must be  >= 0")
-        return self.value
+        self.__value = value
 
 
     
     @property
     def height(self):
-        self.height = height
+        self.__height = height
 
      @property setter
     def height(self, value):
-        self.value = value
+        self.__value = value
         if height != int:
             raise TypeError("height must be an integer")
         elif height < 0:
             raise ValueError("height must be  >= 0")
-        return self.value
+        self.__value = value
