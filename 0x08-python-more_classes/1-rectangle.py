18aed00c8b2377e19ebd4e4341cd57bd3ca24250
dainahwanjiku <deeshish2017@gmail.com>
2022-06-30 03:25:09 +0300

    file

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -9,8 +9,8 @@ class Rectangle:
     rectangle object with getter and setter
     """
     def __init__(self, width=0, height=0):
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
 
     @property
     def width(self):
