97c3d8c341511690b6d56c3e8591e909d46ec8ed
dainahwanjiku <deeshish2017@gmail.com>
2022-07-06 01:13:23 +0300

    file 8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+the class to json function
+"""
+
+
+def class_to_json(obj):
+    """
+    returns the dictionary description with simple data structure
+    """
+    return obj.__dict__
