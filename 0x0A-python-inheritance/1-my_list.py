05bfedc4fe292c3fe4f33056cf4adb0969873ddb
dainahwanjiku <deeshish2017@gmail.com>
2022-07-07 04:40:43 +0300

    file 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -2,11 +2,13 @@
 """
 sorting the list in ascending order
 """
+
+
 class MyList(list):
     """subclass of the list"""
+
     def __init__(self):
         """initializes the object"""
-        super().__init__()
         
     def print_sorted(self):
         """prints the sorted list"""
