0136cf3d02864d090a78e4ac9c8856304d77e555
dainahwanjiku <deeshish2017@gmail.com>
2022-07-12 03:11:23 +0300

    changes

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -334,14 +334,14 @@ class TestRectangle(unittest.TestCase):
         self.assertNotEqual(r, self.r4)
 
         def test_save_to_file(self):
-        """test regular use of save_to_file"""
-        r1 = Rectangle(1, 1, 1, 1, 1)
-        r2 = Rectangle(2, 2, 2, 2, 2)
-        l = [r1, r2]
-        Rectangle.save_to_file(l)
-        with open("Rectangle.json", "r") as f:
-            ls = [r1.to_dictionary(), r2.to_dictionary()]
-            self.assertEqual(json.dumps(ls), f.read())
+            """test regular use of save_to_file"""
+            r1 = Rectangle(1, 1, 1, 1, 1)
+            r2 = Rectangle(2, 2, 2, 2, 2)
+            l = [r1, r2]
+            Rectangle.save_to_file(l)
+            with open("Rectangle.json", "r") as f:
+                ls = [r1.to_dictionary(), r2.to_dictionary()]
+                self.assertEqual(json.dumps(ls), f.read())
 
     def test_stf_empty(self):
         """test save_to_file with empty list"""
