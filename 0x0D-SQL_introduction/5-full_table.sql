64c7f3b413b329672c09a7e0f9dc5a089f60e1cc
dainah <99591839+dainahwanjiku@users.noreply.github.com>
2022-08-12 03:10:15 +0300

    Update 5-full_table.sql

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,5 +1,4 @@
 -- Create a table called 'first_table' in db 'hbtn_0c_0'
 -- first_table should have (id INT) and (name VARCHAR(256)).
 -- If table 'first_table' already exists, should not fail.
--- Not allowed to use SELECT or SHOW.
-CREATE TABLE IF NOT EXISTS first_table(id INT, name VARCHAR(256));
+SHOW CREATE TABLE first_table;
