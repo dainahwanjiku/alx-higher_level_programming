5e1733d402ef36e02703b5d26a83395bc7634a7c
dainahwanjiku <deeshish2017@gmail.com>
2022-09-05 17:42:17 +0300

    first constant first print

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,4 +1,4 @@
-#!/usr/bin/node<F36>
-// prints javascript is amazing without using var keyword.
- let myVar = "Javascript is amazing";
+#!/usr/bin/node
+// prints javascript is amazing without using var keyword
+let myVar = "Javascript is amazing";
 console.log(myVar);
