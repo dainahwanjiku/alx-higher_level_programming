41a718a60b9b3ef297627302e5af376966110917
dainahwanjiku <deeshish2017@gmail.com>
2022-09-06 02:30:48 +0300

    languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,3 @@
 #!/usr/bin/node
-//this script prints three lines of strings
-let myLines = {"C is fun",\n "Python is cool",\n "Javascript is amazing"};
-console.log(myLines);
+'use strict';
+console.log('C is fun\nPython is cool\nJavascript is amazing');
