eb0f9e509cc7060ea3f0186d83a4c368a8f4b420
dainahwanjiku <deeshish2017@gmail.com>
2022-09-06 22:21:53 +0300

    empty rectangle

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,11 +1,2 @@
 #!/usr/bin/node
-class Rectangle {
-	constructor(height, width) {
-		this.height = height;
-		this.width = width;
-	}
-}
-
-const obj = new Rectangle();
-obj.height;
-obj.width;
+module.exports = class Rectangle {};
