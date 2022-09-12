a6c17a2230d13ec5ec2d12a141cc1921b3e7e58e
dainahwanjiku <deeshish2017@gmail.com>
2022-09-12 04:35:50 +0300

    cities by states

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,27 +1,28 @@
 #!/usr/bin/python3
+"""List all cities from the db
+Username, password, and database name given as user args
+Can only use execute() once
+Sort ascending order by cities.id
 """
-python script that lists all cities from the
-database hbtn_0e_4_usa
-"""
-import MySQLdb
 import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=sys.argv[1],
+    db = MySQLdb.connect(user=sys.argv[1],
                          passwd=sys.argv[2],
-                         db=sys.argv[3])
-    cursor = db.cursor()
+                         db=sys.argv[3],
+                         host='localhost',
+                         port=3306)
+    cur = db.cursor()
     cmd = """SELECT cities.id, cities.name, states.name
-          FROM states
-          INNER JOIN cities ON states.id = cities.state_id
-          ORDER BY cities.id ASC"""
-    cursor.execute(cmd)
-    allcities = cursor.fetchal()
-    
+         FROM states
+         INNER JOIN cities ON states.id = cities.state_id
+         ORDER BY cities.id ASC"""
+    cur.execute(cmd)
+    allCities = cur.fetchall()
+
     for city in allCities:
         print(city)
 
-    cursor.close()
+    cur.close()
     db.close()
