41f3bbfda652be348ae2b935b1c8a23ce8c6b5e2
dainahwanjiku <deeshish2017@gmail.com>
2022-09-12 03:43:47 +0300

    fetch first

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
+"""Print the first 'State' object from db 'hbtn_0e_6_usa'
+Script should take 3 args: username, pw, and db name
+Must use SQLAlchemy
 """
-list the first State object from a database
-"""
-import sqlalchemy
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 import sys
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
 from model_state import Base, State
 
 if __name__ == "__main__":
-    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                        .format(argv[1], argv[2], argv[3]))
-    Session = sessionmaker(bind=eng)
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Session = sessionmaker(bind=engine)
     session = Session()
 
     res = session.query(State.id, State.name).first()
