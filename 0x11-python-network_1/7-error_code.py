89a966eb0dc5094ab0558019a454817e201074f6
dainahwanjiku <deeshish2017@gmail.com>
2022-09-23 22:23:19 +0000

    errorcode

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
-Use requests package to make a get request to given URL anddisplay `the body of response, or error code if error.
+takes in a URL, sends a request to the URL & displays the body of the response
 """
-if __name__ == "__main__":
+if __name__ == '__main__':
     import requests
     from sys import argv
     r = requests.get(argv[1])
