5f6d124e468d96fe1fb842c844f2e33f490f47c7
dainahwanjiku <deeshish2017@gmail.com>
2022-09-23 21:24:05 +0000

    post_email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,6 +6,7 @@ decoded in utf-8
 if __name__ == "__main__":
     import urllib.parse as parse
     import urllib.request as request
+    from sys import argv
     values = {'email': sys.argv[2]}
     data = parse.urlencode(values).encode('utf-8')
     req = request.Request(sys.argv[1], data)
