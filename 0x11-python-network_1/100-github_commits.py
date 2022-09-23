746053015f9c0d9db6399fb3b3d3964fa67a4ec3
dainahwanjiku <deeshish2017@gmail.com>
2022-09-23 23:14:58 +0000

    github commit

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""Lists the 10 most recent commits on a given GitHub repository.
-Arguments: arg1: repo name, arg2: repo owner
 """
-if __name__ == "__main__":
-    from sys import argv
+list 10 commits (from the most recent to oldest) of the repository and user
+sent in as arguments
+"""
+if __name__ == '__main__':
     import requests
-    # Get the arguments, arg1: repo name, arg2: repo owner
-    url = requests.get("https://api.github.com/repos/{}/{}/commits".format(
-            sys.argv[2], sys.argv[1]))
+    from sys import argv
+    r = requests.get('https://api.github.com/repos/{}/{}/commits'
+                     .format(argv[2], argv[1]))
     commits = r.json()
     for commit in commits[:10]:
-        print(commit.get('sha'), end=':')
+        print(commit.get('sha'), end=': ')
         print(commit.get('commit').get('author').get('name'))
