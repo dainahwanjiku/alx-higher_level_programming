5c561c7c80e46a2df549f9e7be2af07338a25a06
dainahwanjiku <deeshish2017@gmail.com>
2022-09-23 22:52:16 +0000

    json

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user
-with the letter as a parameter
+with the letter as a parameter.
 """
 if __name__ == '__main__':
     import requests
