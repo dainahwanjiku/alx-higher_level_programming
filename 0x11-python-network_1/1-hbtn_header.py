9e27e211ce46fbed7d14b7dc5fbd8f14ec5b99b8
dainahwanjiku <deeshish2017@gmail.com>
2022-09-23 21:42:03 +0300

    hbtn header

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""Take in a URL, send request to URL and display value of `X-Request-Id`"""
-import sys
-from urllib import request
-
+"""
+Take in a URL, send request to URL and display value of `X-Request-Id`
+"""
 if __name__ == "__main__":
+    import urllib.request as request
+    from sys import argv
     with request.urlopen(sys.argv[1]) as res:
         print(res.info()['X-Request-Id'])
