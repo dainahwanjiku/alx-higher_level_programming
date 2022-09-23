b4b4f3ce4d1e7da8cbba7ce20618ceed45ce3a36
dainahwanjiku <deeshish2017@gmail.com>
2022-09-23 22:29:47 +0000

    json

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,26 +1,23 @@
 #!/usr/bin/python3
 """
-Use requests package to make a post request to given URL with argument
-set in variable `q`, defaulting to empty string. If response body is properly
-JSON formatted and not empty, display `id` and `name` as given format.
-Otherwise display error message.
+takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter
 """
-if __name__ == "__main__":
+if __name__ == '__main__':
     import requests
     from sys import argv
-    if len(sys.argv) > 1:
-        arg = sys.argv[1]
+    if len(argv) == 2:
+        q = argv[1]
     else:
-        arg = ""
-    payload = {'q': arg}
-    url = "http://0.0.0.0:5000/search_user"
-    r = requests.post(url, data=payload)
+        q = ""
+    r = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
     try:
-        r.raise_for_status()
-        json = r.json()
-        if len(json) == 0:
+        r_dict = r.json()
+        id = r_dict.get('id')
+        name = r_dict.get('name')
+        if len(r_dict) == 0 or not id or not name:
             print("No result")
         else:
-            print("[{:d}] {}".format(json['id'], json['name']))
-    except Exception:
+            print("[{}] {}".format(r_dict.get('id'), r_dict.get('name')))
+    except:
         print("Not a valid JSON")
